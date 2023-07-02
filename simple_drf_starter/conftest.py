42adf1efadbb348e40c4934e07fad121c7b03aed
dongbox <sfreebobo@163.com>
2023-07-02 15:30:23 +0800

    通过cookiecutter-django初始化一个项目结构

---
+++ simple_drf_starter/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from simple_drf_starter.users.models import User
+from simple_drf_starter.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
